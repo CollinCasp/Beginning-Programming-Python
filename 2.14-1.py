dff544e5bbe757b0b2d6a4bba73cd9de465ac194
CollinCasp <cjcordsen@mavs.coloradomesa.edu>
2025-09-02 14:06:55 -0600

    Add files via upload

---
+++ 2.14-1.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      cjcordsen
+#
+# Created:     02/09/2025
+# Copyright:   (c) cjcordsen 2025
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+All = "All "
+Work = "work "
+And = "and "
+No = "no "
+Play = "play "
+Make = "makes "
+Jack = "Jack "
+A = "a "
+Dull = "dull "
+Boy = "boy."
+print (All + Work + And + No + Play + Make + Jack + A + Dull + Boy)
