38498c4f34228daf3e0cdc697b9212e81d09a32d
CollinCasp <cjcordsen@mavs.coloradomesa.edu>
2025-09-23 14:03:13 -0600

    Add files via upload

---
+++ 5.14.1.py
@@ -0,0 +1,34 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      cjcordsen
+#
+# Created:     23/09/2025
+# Copyright:   (c) cjcordsen 2025
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+
+day = int(input("Input Number Between 0 - 6"))
+
+if day == 0:
+    print ("Today is Sunday")
+elif day == 1:
+    print ("Today is Monday")
+elif day == 2:
+    print ("Today is Tuesday")
+elif day == 3:
+    print ("Today is Wednesday")
+elif day == 4:
+    print ("Today is Thursday")
+elif day == 5:
+    print ("Today is Friday")
+elif day == 6:
+    print ("Today is Saturday")
+else:
+    print ("Invalid Number")
+
